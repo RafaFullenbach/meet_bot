884b2821f1977820016f3c67d4eae46109170063
Rafael Carvalho Füllenbach <rafael.fullenbach12@gmail.com>
2024-09-16 18:18:14 -0300

    feat: homepage setup

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['flagpedia.net'],
-    }
-};
-
-export default nextConfig;
+      domains: ['flagpedia.net'],
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/signIn',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
